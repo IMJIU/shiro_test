8e3e3fc066818685fb5079c4299a46b036b2c644
zlfkyo@sina.com <zlfkyo@sina.com>
2016-09-30 00:08:32 +0800

    j

--- src/main/java/com/github/zhangkaitao/shiro/chapter12/service/PasswordHelper.java
+++ src/main/java/com/github/zhangkaitao/shiro/chapter12/service/PasswordHelper.java
@@ -9,39 +9,39 @@ import org.apache.shiro.util.ByteSource;
 import org.springframework.beans.factory.annotation.Autowired;
 
 /**
- * <p>User: Zhang Kaitao
- * <p>Date: 14-1-28
- * <p>Version: 1.0
+ * <p>
+ * User: Zhang Kaitao
+ * <p>
+ * Date: 14-1-28
+ * <p>
+ * Version: 1.0
  */
 public class PasswordHelper {
-    @Autowired
-    private RetryLimitHashedCredentialsMatcher credentialsMatcher;
-    private RandomNumberGenerator randomNumberGenerator = new SecureRandomNumberGenerator();
-    private String algorithmName = "md5";
-    private int hashIterations = 2;
+//	@Autowired
+//	private RetryLimitHashedCredentialsMatcher credentialsMatcher;
+	private RandomNumberGenerator randomNumberGenerator = new SecureRandomNumberGenerator();
+	private String algorithmName = "md5";
+	private int hashIterations = 2;
 
-    public void setRandomNumberGenerator(RandomNumberGenerator randomNumberGenerator) {
-        this.randomNumberGenerator = randomNumberGenerator;
-    }
+	public void setRandomNumberGenerator(RandomNumberGenerator randomNumberGenerator) {
+		this.randomNumberGenerator = randomNumberGenerator;
+	}
 
-    public void setAlgorithmName(String algorithmName) {
-        this.algorithmName = algorithmName;
-    }
+	public void setAlgorithmName(String algorithmName) {
+		this.algorithmName = algorithmName;
+	}
 
-    public void setHashIterations(int hashIterations) {
-        this.hashIterations = hashIterations;
-    }
+	public void setHashIterations(int hashIterations) {
+		this.hashIterations = hashIterations;
+	}
 
-    public void encryptPassword(User user) {
+	public void encryptPassword(User user) {
 
-        user.setSalt(randomNumberGenerator.nextBytes().toHex());
-        user.setPassword(credentialsMatcher.toPwd(user.getPassword().toCharArray(),ByteSource.Util.bytes(user.getCredentialsSalt()),2));
-//        String newPassword = new SimpleHash(
-//                algorithmName,
-//                user.getPassword(),
-//                ByteSource.Util.bytes(user.getCredentialsSalt()),
-//                hashIterations).toHex();
-
-//        user.setPassword(newPassword);
-    }
+		user.setSalt(randomNumberGenerator.nextBytes().toHex());
+//		user.setPassword(credentialsMatcher.toPwd(user.getPassword().toCharArray(),
+//				ByteSource.Util.bytes(user.getCredentialsSalt()), credentialsMatcher.getHashIterations()));
+		String newPassword = new SimpleHash(algorithmName, user.getPassword(),
+				ByteSource.Util.bytes(user.getCredentialsSalt()), hashIterations).toHex();
+		user.setPassword(newPassword);
+	}
 }
